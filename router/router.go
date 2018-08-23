c5798308204684dcd912bf269f7505ae35d88ff3
Hugo Sykes <hugo@denmeadfarm.co.uk>
2018-08-23 23:27:13 +0100

    Revert "Written router as a proxy into the server"
    This reverts commit 1f40e13b146d050496fcd6da9aa014d5acaff9e2.

--- router/router.go
+++ router/router.go
@@ -1,40 +1,20 @@
 package main
 
 import (
-	//"github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"html"
-	//"fmt"
-	"bytes"
 	"fmt"
-	"io/ioutil"
+	"html"
 )
 
 func main() {
-	//router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", Index)
-	http.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:8080"+html.EscapeString(r.URL.Path), bytes.NewBuffer([]byte(html.EscapeString(r.URL.Path))))
-
-	req.Header.Set("Content-Type", "text/plain")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Fprint(w,"response Status: ", resp.Status)
-	fmt.Fprint(w,"response Headers:", resp.Header)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response body is:", string(body))
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
